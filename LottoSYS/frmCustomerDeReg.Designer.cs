f1adacd697a79f1c0d1a8870c8ac9b9ce6796a9a
t00168162 <t00168162@1XXF48E3874C601>
2016-10-13 12:00:16 +0100

    Dereg new

--- LottoSYS/frmCustomerDeReg.Designer.cs
+++ LottoSYS/frmCustomerDeReg.Designer.cs
@@ -31,7 +31,25 @@
             this.mnuUpdateCustomer = new System.Windows.Forms.MenuStrip();
             this.mnuBack = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblDeReg = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearchBox = new System.Windows.Forms.TextBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtForename = new System.Windows.Forms.TextBox();
+            this.lblForename = new System.Windows.Forms.Label();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.txtTown = new System.Windows.Forms.TextBox();
+            this.lblTown = new System.Windows.Forms.Label();
+            this.txtAddress2 = new System.Windows.Forms.TextBox();
+            this.lblAddressLine2 = new System.Windows.Forms.Label();
+            this.txtAddress1 = new System.Windows.Forms.TextBox();
+            this.lblAddressLine1 = new System.Windows.Forms.Label();
+            this.chkDeceased = new System.Windows.Forms.CheckBox();
+            this.chkWithdrawn = new System.Windows.Forms.CheckBox();
             this.mnuUpdateCustomer.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // mnuUpdateCustomer
@@ -41,34 +59,218 @@
             this.exitToolStripMenuItem});
             this.mnuUpdateCustomer.Location = new System.Drawing.Point(0, 0);
             this.mnuUpdateCustomer.Name = "mnuUpdateCustomer";
-            this.mnuUpdateCustomer.Size = new System.Drawing.Size(651, 24);
+            this.mnuUpdateCustomer.Padding = new System.Windows.Forms.Padding(12, 4, 0, 4);
+            this.mnuUpdateCustomer.Size = new System.Drawing.Size(784, 27);
             this.mnuUpdateCustomer.TabIndex = 1;
             this.mnuUpdateCustomer.Text = "menuStrip1";
             // 
             // mnuBack
             // 
             this.mnuBack.Name = "mnuBack";
-            this.mnuBack.Size = new System.Drawing.Size(44, 20);
+            this.mnuBack.Size = new System.Drawing.Size(44, 19);
             this.mnuBack.Text = "Back";
             this.mnuBack.Click += new System.EventHandler(this.mnuBack_Click);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 19);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
+            // lblDeReg
+            // 
+            this.lblDeReg.AutoSize = true;
+            this.lblDeReg.Location = new System.Drawing.Point(51, 52);
+            this.lblDeReg.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblDeReg.Name = "lblDeReg";
+            this.lblDeReg.Size = new System.Drawing.Size(130, 25);
+            this.lblDeReg.TabIndex = 2;
+            this.lblDeReg.Text = "De - register";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(238, 101);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // txtSearchBox
+            // 
+            this.txtSearchBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearchBox.Location = new System.Drawing.Point(112, 104);
+            this.txtSearchBox.Name = "txtSearchBox";
+            this.txtSearchBox.Size = new System.Drawing.Size(100, 22);
+            this.txtSearchBox.TabIndex = 5;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSearch.Location = new System.Drawing.Point(35, 109);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(68, 15);
+            this.lblSearch.TabIndex = 4;
+            this.lblSearch.Text = "Id Number:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkWithdrawn);
+            this.groupBox1.Controls.Add(this.chkDeceased);
+            this.groupBox1.Controls.Add(this.txtForename);
+            this.groupBox1.Controls.Add(this.lblForename);
+            this.groupBox1.Controls.Add(this.txtSurname);
+            this.groupBox1.Controls.Add(this.lblSurname);
+            this.groupBox1.Controls.Add(this.txtTown);
+            this.groupBox1.Controls.Add(this.lblTown);
+            this.groupBox1.Controls.Add(this.txtAddress2);
+            this.groupBox1.Controls.Add(this.lblAddressLine2);
+            this.groupBox1.Controls.Add(this.txtAddress1);
+            this.groupBox1.Controls.Add(this.lblAddressLine1);
+            this.groupBox1.Location = new System.Drawing.Point(38, 173);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(499, 258);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // txtForename
+            // 
+            this.txtForename.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtForename.Location = new System.Drawing.Point(139, 66);
+            this.txtForename.Name = "txtForename";
+            this.txtForename.Size = new System.Drawing.Size(136, 21);
+            this.txtForename.TabIndex = 65;
+            // 
+            // lblForename
+            // 
+            this.lblForename.AutoSize = true;
+            this.lblForename.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblForename.Location = new System.Drawing.Point(52, 72);
+            this.lblForename.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblForename.Name = "lblForename";
+            this.lblForename.Size = new System.Drawing.Size(75, 15);
+            this.lblForename.TabIndex = 64;
+            this.lblForename.Text = "Forename: *";
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSurname.Location = new System.Drawing.Point(139, 29);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(136, 21);
+            this.txtSurname.TabIndex = 63;
+            // 
+            // lblSurname
+            // 
+            this.lblSurname.AutoSize = true;
+            this.lblSurname.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSurname.Location = new System.Drawing.Point(58, 35);
+            this.lblSurname.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSurname.Name = "lblSurname";
+            this.lblSurname.Size = new System.Drawing.Size(69, 15);
+            this.lblSurname.TabIndex = 62;
+            this.lblSurname.Text = "Surname: *";
+            // 
+            // txtTown
+            // 
+            this.txtTown.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTown.Location = new System.Drawing.Point(139, 181);
+            this.txtTown.Name = "txtTown";
+            this.txtTown.Size = new System.Drawing.Size(136, 22);
+            this.txtTown.TabIndex = 61;
+            // 
+            // lblTown
+            // 
+            this.lblTown.AutoSize = true;
+            this.lblTown.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTown.Location = new System.Drawing.Point(79, 187);
+            this.lblTown.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTown.Name = "lblTown";
+            this.lblTown.Size = new System.Drawing.Size(48, 15);
+            this.lblTown.TabIndex = 60;
+            this.lblTown.Text = "Town: *";
+            // 
+            // txtAddress2
+            // 
+            this.txtAddress2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddress2.Location = new System.Drawing.Point(139, 142);
+            this.txtAddress2.Name = "txtAddress2";
+            this.txtAddress2.Size = new System.Drawing.Size(136, 22);
+            this.txtAddress2.TabIndex = 59;
+            // 
+            // lblAddressLine2
+            // 
+            this.lblAddressLine2.AutoSize = true;
+            this.lblAddressLine2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddressLine2.Location = new System.Drawing.Point(28, 147);
+            this.lblAddressLine2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAddressLine2.Name = "lblAddressLine2";
+            this.lblAddressLine2.Size = new System.Drawing.Size(99, 15);
+            this.lblAddressLine2.TabIndex = 58;
+            this.lblAddressLine2.Text = "Address Line 2: *";
+            // 
+            // txtAddress1
+            // 
+            this.txtAddress1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddress1.Location = new System.Drawing.Point(139, 104);
+            this.txtAddress1.Name = "txtAddress1";
+            this.txtAddress1.Size = new System.Drawing.Size(136, 22);
+            this.txtAddress1.TabIndex = 57;
+            // 
+            // lblAddressLine1
+            // 
+            this.lblAddressLine1.AutoSize = true;
+            this.lblAddressLine1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddressLine1.Location = new System.Drawing.Point(28, 109);
+            this.lblAddressLine1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAddressLine1.Name = "lblAddressLine1";
+            this.lblAddressLine1.Size = new System.Drawing.Size(99, 15);
+            this.lblAddressLine1.TabIndex = 56;
+            this.lblAddressLine1.Text = "Address Line 1: *";
+            // 
+            // chkDeceased
+            // 
+            this.chkDeceased.AutoSize = true;
+            this.chkDeceased.Location = new System.Drawing.Point(317, 66);
+            this.chkDeceased.Name = "chkDeceased";
+            this.chkDeceased.Size = new System.Drawing.Size(128, 29);
+            this.chkDeceased.TabIndex = 66;
+            this.chkDeceased.Text = "Deceased";
+            this.chkDeceased.UseVisualStyleBackColor = true;
+            // 
+            // chkWithdrawn
+            // 
+            this.chkWithdrawn.AutoSize = true;
+            this.chkWithdrawn.Location = new System.Drawing.Point(317, 137);
+            this.chkWithdrawn.Name = "chkWithdrawn";
+            this.chkWithdrawn.Size = new System.Drawing.Size(132, 29);
+            this.chkWithdrawn.TabIndex = 67;
+            this.chkWithdrawn.Text = "Withdrawn";
+            this.chkWithdrawn.UseVisualStyleBackColor = true;
+            // 
             // frmCustomerDeReg
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(651, 477);
+            this.ClientSize = new System.Drawing.Size(784, 462);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearchBox);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.lblDeReg);
             this.Controls.Add(this.mnuUpdateCustomer);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "frmCustomerDeReg";
             this.Text = "frmCustomerDeReg";
             this.mnuUpdateCustomer.ResumeLayout(false);
             this.mnuUpdateCustomer.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,5 +281,22 @@
         private System.Windows.Forms.MenuStrip mnuUpdateCustomer;
         private System.Windows.Forms.ToolStripMenuItem mnuBack;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label lblDeReg;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearchBox;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chkWithdrawn;
+        private System.Windows.Forms.CheckBox chkDeceased;
+        private System.Windows.Forms.TextBox txtForename;
+        private System.Windows.Forms.Label lblForename;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.TextBox txtTown;
+        private System.Windows.Forms.Label lblTown;
+        private System.Windows.Forms.TextBox txtAddress2;
+        private System.Windows.Forms.Label lblAddressLine2;
+        private System.Windows.Forms.TextBox txtAddress1;
+        private System.Windows.Forms.Label lblAddressLine1;
     }
 }
